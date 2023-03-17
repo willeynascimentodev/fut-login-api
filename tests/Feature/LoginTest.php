a4af977778f45835859b9bb89408d8637110b39c
Willey Nascimento <willeynascimentodev@waan.com.br>
2023-03-17 15:55:19 -0300

    Test Cases

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -13,10 +13,47 @@ class LoginTest extends TestCase
      *
      * @return void
      */
-    public function loginTest()
+    public function testGenerateAccessToken()
     {
-        // $response = $this->get('/');
+        $data = [
+            'email' => 'willeynascimentooficial@gmail.com',
+            'password' => 'abcds',
+        ];
 
-        // $response->assertStatus(200);
+        $response = $this->post('api/login', $data);
+        $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'data' ,
+            'message'
+        ]);
+    }
+
+    public function testInvalidUser()
+    {
+        $data = [
+            'email' => 'willeynascimentooficial@gmail.com',
+            'password' => 'abcd',
+        ];
+
+        $response = $this->post('api/login', $data);
+        $response->assertStatus(401);
+        $response->assertJsonStructure([
+            'data',
+            'message'
+        ]);
+    }
+
+    public function testInvalidData()
+    {
+        $data = [
+            'email' => 'willeynascimentooficial@gmail.com',
+            'passwor' => 'abcds',
+        ];
+
+        $response = $this->post('api/login', $data);
+        $response->assertStatus(400);
+        $response->assertJsonStructure([
+            'message'
+        ]);
     }
 }
