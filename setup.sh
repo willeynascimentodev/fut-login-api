2a5f9651231a025b545a24b7c9176ade6e6060c8
Willey Nascimento <willeynascimentodev@waan.com.br>
2023-04-05 13:33:17 -0300

    Updating Set Up Process

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 composer install
 docker compose up -d
+docker compose exec -i fut-login-api cp env_example_local .env
 docker compose exec -i fut-login-api php artisan config:clear
-docker compose exec -i fut-login-api php artisan migrate:fresh
-docker compose exec -i fut-login-api php artisan db:seed
+docker compose exec -i fut-login-api php artisan migrate:fresh --env=local
+docker compose exec -i fut-login-api php artisan db:seed --env=local
